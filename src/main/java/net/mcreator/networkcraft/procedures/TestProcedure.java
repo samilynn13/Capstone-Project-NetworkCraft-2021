f97a853dfe63eb00661a27fe4e70b914ff9e3491
samilynn13 <sami0513@yahoo.com>
2021-09-14 15:42:18 -0700

    Commit Test 3

---
+++ src/main/java/net/mcreator/networkcraft/procedures/TestProcedure.java
@@ -0,0 +1,15 @@
+package net.mcreator.networkcraft.procedures;
+
+import net.mcreator.networkcraft.NetworkcraftModElements;
+
+import java.util.Map;
+
+@NetworkcraftModElements.ModElement.Tag
+public class TestProcedure extends NetworkcraftModElements.ModElement {
+	public TestProcedure(NetworkcraftModElements instance) {
+		super(instance, 9);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+	}
+}
